9d080706087dc1c2e51858ff15af651979e6dd7a
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2020-08-07 23:11:45 +0300

    Initial Commit

---
+++ MyFirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUI
+//
+//  Created by Seyfeddin Bassarac on 7.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Seyfeddin Bassarac on 7.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
